8546c91a907935b6136b7234157b0e000063b876
Ray Farias <ray@rayfarias.com>
2015-01-16 10:22:41 -1000

    add (build_arrays.js): add work file for students

---
+++ build_arrays.js
@@ -0,0 +1,132 @@
+
+
+/*
+ * LEVEL 1 - Standard Arrays
+ **/
+
+// Create an empty array.
+
+
+// Create an array of the colors of the rainbow.
+
+
+// Create an array of gems.
+
+
+// Create an array of military ranks.
+
+
+// Create an array of road signs.
+
+
+// Create an array containing components of a tree.
+
+
+// Create an array of prime numbers less than 20.
+
+
+// Create an array of words programmers typically say while programming.
+
+
+// Create an array of elements of the periodic table.
+
+
+// Create an array representing the coins currently in your pocket.
+
+
+// Create an array of items you bring to class daily.
+
+
+// Create an array of web developer role titles.
+
+
+// Create an array of good luck charms.
+
+
+// Create an array of mythical animals.
+
+
+// Create an array of drinks.
+
+
+// Create an array of emoticons.
+
+
+// Create an array with the letters of the word 'array'.
+
+
+
+
+/*
+ * LEVEL 2 - Object Arrays
+ **/
+
+// Create an array of Objects that represent Famous People.
+// Each Object should have a Name, Age, and their Profession as properties
+
+
+// Create an array of songs you'd like Jon to add to the playlist of music 
+// to listen to in class.  Each song should be an object with at least 
+// a title and an artist.
+
+
+// Create an array of functions. Define any functions you want, they can 
+// be anything! But make sure the syntax is correct :D
+
+
+// Create an array of Sublime shortcuts.  Each item should have a keystroke 
+// combination and a textual description of what the key combination does. 
+// You can find Sublime Text Shortcuts by traversing the Sublime Text's GUI
+// e.g. File Menu options.
+
+
+
+
+/*
+ * LEVEL 3 - Nested Arrays
+ **/
+
+// Create an array of gem arrays, where each gem array contains gems that 
+// are of a similar color.
+
+
+// Create an array of aisles at a supermarket.  Each aisle is an array of 
+// items you might find in an aisle together.  (e.g. flour and baking 
+// powder would be found in an aisle together so would Apples and Bananas.)
+
+
+// Create an array of three arrays, each array having numbers that add up 
+// to 15, but having no element listed more than once in any array.
+// e.g. each element can only be in one array, and at most once in each array
+
+
+
+
+/*
+ * LEVEL 4 - 
+ **/
+
+// Create an array with three elements, each consisting of a single 
+// character: single quotes, double quotes, and a comma.
+
+
+// Create an array of 1000 elements.  
+// (Contents do not matter, it just needs to have 1000 elements.)
+
+
+// Create an array of three elements.  
+// The first and second elements should not be equal, the second and 
+// third elements should not be equal, but the first and third elements 
+// should be equal.
+
+
+// Create an array of at least five elements where each element is equal 
+// to the elements adjacent to it.
+
+
+// Create an array of at least five reserved Javascript keywords.
+
+
+// Create an array of an array of an array of an array of an array of an 
+// array containing a single element: the number 42.
+
