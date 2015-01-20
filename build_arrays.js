c8607e785adb7a8d065d39ea2fee0f4a31b6a82a
Jon Borgonia <jon@gomagames.com>
2015-01-19 14:47:58 -1000

    adds level 2

--- build_arrays.js
+++ build_arrays.js
@@ -54,3 +54,28 @@
 
 
 
+
+/*
+ * LEVEL 2 - Object Arrays
+ **/
+
+// Create an array of Objects that represent Famous People.
+// Each Object should have a Name, Age, and their Profession as properties
+
+
+// Create an array of songs you'd like Jon to add to the playlist of music 
+// to listen to in class.  Each song should be an object with at least 
+// a title and an artist.
+
+
+// Create an array of functions. Define any functions you want, they can 
+// be anything! But make sure the syntax is correct :D
+
+
+// Create an array of Sublime shortcuts.  Each item should have a keystroke 
+// combination and a textual description of what the key combination does. 
+// You can find Sublime Text Shortcuts by traversing the Sublime Text's GUI
+// e.g. File Menu options.
+
+
+
