4d675adca3e941b645c896bc65a27e89a366e917
yukio808 <aj.yamamoto@me.com>
2015-01-20 18:38:13 -1000

    added all personal variables

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,49 @@
  **/
 
 // Create an empty array.
-
-
+var array1 = [];
 // Create an array of the colors of the rainbow.
-
+var arrainbows = [red, orange, yellow, green, blue, indigo, violet];
 
 // Create an array of gems.
-
+var arraygems = [emarald, ruby, saphire, quarts, amethist, diamond, tourmaline, jade, opal];
 
 // Create an array of military ranks.
-
+var navalranks =["seaman recruit","seaman aprentice", "petty officer third class", "petty officer second class", "petty officer first class", "chief petty officer", "senior chief petty officer", "master chief", "master chief of the navy", "ensign", "lieutenant JG", "lieutenant", "lieutanant commander"," commander", "captain", "rear admiral lower half", "rear admiral upper half","vice admiral","admiral"];
 
 // Create an array of road signs.
-
+var roadsignarray = ["stop", "hazzard"]
 
 // Create an array containing components of a tree.
-
+var elmentsarray = ["carbon"]
 
 // Create an array of prime numbers less than 20.
-
+var primenumb = [1, 3, 5 ,7, 11, 13, 17, 19]
 
 // Create an array of words programmers typically say while programming.
-
+var wordsforfun = ["damnit", "say what", "son of a *bleep*"]
 
 // Create an array of elements of the periodic table.
-
+var periodicele = ["galium","plutonium ","hydrogen"]
 
 // Create an array representing the coins currently in your pocket.
-
+var coins = []
 
 // Create an array of items you bring to class daily.
-
+var essentials =["computer", "food", "good attitude"]
 
 // Create an array of web developer role titles.
-
-
+var roles = ["front end", "backend"];
 // Create an array of good luck charms.
-
-
+var charms = ["angel coin"]
 // Create an array of mythical animals.
-
+var legendary =["raiku", "entei", "suicune"];
 
 // Create an array of drinks.
-
+var drinks = ["ramune","soda","juice","WATER"];
 
 // Create an array of emoticons.
-
+var emoticons = ["(smile)"];
 
 // Create an array with the letters of the word 'array'.
-
-
-
+var array = ["a", "r", "r", "a", "y"];
